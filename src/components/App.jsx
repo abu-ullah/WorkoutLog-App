5c50f2a75908d4c8cfbea75200368d189191ba93
abu-ullah <abu.ullah03@gmail.com>
2023-07-25 10:47:27 -0400

    Initial Commit, created Header, footer, and started log component

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './Header';
+import Log from './Log';
+import Footer from './Footer';
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Log />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Log.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Log() {
+    return (
+        <div className="log">
+            <h1>This is the title</h1>
+            <p>This is the content</p>
+        </div>
+    );
+}
+
+export default Log;
